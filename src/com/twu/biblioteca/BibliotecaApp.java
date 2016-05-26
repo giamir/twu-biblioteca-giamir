b9ed0476f61e6e262cc402ac0ca9e04274862f06
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2016-05-26 17:40:20 +0100

    show a main menu with options to customer

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -13,17 +13,24 @@ public class BibliotecaApp {
     public void go() {
         Message message = new Message(System.out);
         message.printWelcome();
-        ArrayList<Book> bookList = makeBookList();
-        BookLister bookLister = new BookLister(bookList);
-        bookLister.listBooks();
+        BookLister bookLister = new BookLister(makeBookList());
+        MainMenu mainMenu = new MainMenu(makeMenuItems());
+        mainMenu.printListOptions(System.out);
     }
 
-    private ArrayList<Book> makeBookList(){
+    private ArrayList<Book> makeBookList() {
         Book b1 = new Book("High Fidelity", " Nick Hornby", 1995);
         Book b2 = new Book("The Divine Comedy", "Dante Alighieri", 1320);
         Book b3 = new Book("The Decameron", "Giovanni Boccaccio", 1353);
         ArrayList<Book> bookList = new ArrayList<Book>();
         bookList.addAll(Arrays.asList(b1, b2, b3));
         return bookList;
+    }
+
+    private ArrayList<MenuItem> makeMenuItems() {
+        ListOption listBooks = new ListOption("List Books");
+        ArrayList<MenuItem> items = new ArrayList<MenuItem>();
+        items.add(listBooks);
+        return items;
     }
 }
--- test/com/twu/biblioteca/MainMenuTest.java
+++ test/com/twu/biblioteca/MainMenuTest.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class MainMenuTest {
