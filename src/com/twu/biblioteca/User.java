de7b3d636a289ba9d9a0d55603e5f0956d540bb6
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2016-05-29 08:56:02 +0100

    implement User class

---
+++ src/com/twu/biblioteca/User.java
@@ -0,0 +1,19 @@
+package com.twu.biblioteca;
+
+public class User {
+    private String libraryNumber;
+    private String password;
+
+    public User(String ln, String pwd) {
+        libraryNumber = ln;
+        password = pwd;
+    }
+
+    public String getLibraryNumber() {
+        return libraryNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ test/com/twu/biblioteca/UserTest.java
@@ -0,0 +1,25 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UserTest {
+    private User user;
+
+    @Before
+    public void beforeEach() {
+        user = new User("123-4567", "password");
+    }
+
+    @Test
+    public void getLibraryNumberShouldRetrieveTheUserLibraryNumber() {
+        assertEquals("123-4567", user.getLibraryNumber());
+    }
+
+    @Test
+    public void getPasswordShouldRetrieveTheUserPassword() {
+        assertEquals("password", user.getPassword());
+    }
+}
+
